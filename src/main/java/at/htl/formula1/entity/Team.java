b2a5865c489d0e5d97635454c64713f91a5d790d
halilbahar <halil.s.bahar@gmail.com>
2019-11-15 14:09:11 +0100

    Fix generating type from sequence to identity

--- src/main/java/at/htl/formula1/entity/Team.java
+++ src/main/java/at/htl/formula1/entity/Team.java
@@ -18,7 +18,7 @@ import javax.persistence.*;
 public class Team {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
